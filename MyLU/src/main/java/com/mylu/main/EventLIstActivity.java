cbe8c23b40c96a5b8e6faa0e92337197858b32bb
hansenzhang <hansen.s.zhang@gmail.com>
2014-01-16 13:16:33 -0500

    added internet activity to manifest

---
+++ MyLU/src/main/java/com/mylu/main/EventLIstActivity.java
@@ -0,0 +1,22 @@
+package com.mylu.main;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import org.json.JSONObject;
+
+/**
+ * Created by hansen on 1/15/14.
+ */
+public class EventListActivity extends FragmentActivity implements JSONParser.JSONParserCallback{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void showList(JSONObject result) {
+
+    }
+}
